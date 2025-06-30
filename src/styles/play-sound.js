17c53ae06645056fd44cb8b58f58b94fadcabb87
Hidemi Claros <146749783+hclaros-dawi@users.noreply.github.com>
2025-06-30 16:00:08 +0200

    Delete src/styles/play-sound.js

--- src/styles/play-sound.js
+++
@@ -1,6 +0,0 @@
-export default function playSound (sound, volume = 0.25) {
-	if (localStorage.getItem('sound') === 'false') return
-	const audio = new Audio(`/sounds/${sound}`)
-	audio.volume = volume
-	audio.play()
-}
